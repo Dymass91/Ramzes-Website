00d1fa9bd1a3b43a5568513138a2d366c5458bcd
Tomasz Matyszczak <Matyszczak24@gmail.com>
2020-01-30 08:47:43 +0000

    positioning of btns after click

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-$(document).ready(function () {
-    $('.toggle').click(function () {
-        $('.toggle').toggleClass('active')
-        $('.overlay').toggleClass('active')
-        $('.menu').toggleClass('active')
-    })
-})
+
+const toggle = document.querySelector('.toggle')
+const overlay = document.querySelector('.overlay')
+const btnBlog = document.querySelector('.btn__blog')
+const active = document.querySelector('.active')
+const buttons = document.querySelector('.buttons')
+const btnProject = document.querySelector('.btn__projects')
+
+const expand = () => {
+
+    overlay.classList.toggle('active');
+    btnBlog.classList.toggle('active');
+    btnProject.classList.toggle('active');
+    buttons.classList.toggle('active');
+};
+
+
+btnBlog.addEventListener('click', expand);
