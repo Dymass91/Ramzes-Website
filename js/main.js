4481d2b17099e78d46981ca700fdbbd95640046a
Tomasz Matyszczak <Matyszczak24@gmail.com>
2020-02-17 20:45:19 +0000

    looking for solution with aos slide

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ const buttons = document.querySelector('.buttons')
 const btnProject = document.querySelector('.btn__projects')
 const navbar = document.querySelector('.topnav')
 
+const body = document.querySelector('body');
+const btnRefresh = document.querySelector('.refresh')
+
 const expand = () => {
 
     overlay.classList.toggle('active');
@@ -28,49 +31,6 @@ const expand = () => {
 
 };
 
-
-
-
-// function debounce(func, wait = 20, immediate = true) {
-//     var timeout;
-//     return function () {
-//         var context = this, args = arguments;
-//         var later = function () {
-//             timeout = null;
-//             if (!immediate) func.apply(context, args);
-//         };
-//         var callNow = immediate && !timeout;
-//         clearTimeout(timeout);
-//         timeout = setTimeout(later, wait);
-//         if (callNow) func.apply(context, args);
-//     };
-// };
-
-// const sliderPosts = document.querySelectorAll('.slide-in');
-
-// function checkSlide() {
-
-//     sliderPosts.forEach(sliderPost => {
-//         // half way through the image
-//         const slideInAt = (window.scrollY + window.innerHeight);
-//         sliderPost.height / 2;
-//         // bottom of the image
-//         const postBottom = sliderPosts.offsetTop + sliderPost.height;
-//         const isHalfShown = slideInAt > sliderPost.offsetTop;
-//         const isNotScrolledPast = window.scrollY < postBottom;
-//         if (isHalfShown && isNotScrolledPast) {
-//             sliderPost.classList.add('active')
-//             console.log('nie dziala')
-//         }
-//         else {
-//             sliderPost.classList.remove('active')
-//             console.log(' dziala')
-//         }
-//     });
-// }
-
-
-//window.addEventListener('scroll', debounce(checkSlide));
 btnBlog.addEventListener('click', expand);
 
 // if (btnBlog == active) {
@@ -84,3 +44,6 @@ btnBlog.addEventListener('click', expand);
 
 //     overlay.style.height = '0%';
 // }
+
+
+
