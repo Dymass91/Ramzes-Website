b0992b2c72c78143e32ee72686362f18dd857445
Tomasz Matyszczak <Matyszczak24@gmail.com>
2020-03-21 17:35:18 +0000

    Add posts funtion in new js file

---
+++ js/AddPost.js
@@ -0,0 +1,34 @@
+
+const form = document.querySelector('form');
+const PostNumber = document.querySelector('h1 span');
+const listItems = document.getElementsByClassName('Post');
+const ul = document.querySelector('ul');
+const input = document.querySelector('input');
+
+const removePost = (e) => {
+    e.target.parentNode.remove();
+    taskNumber.textContent = listItems.length;
+    console.log("titlePost");
+}
+
+const addPost = (e) => {
+    e.preventDefault()
+    const titlePost = input.value;
+    console.log(titlePost);
+
+    if (titleTask === "") return;
+
+
+    const Post = document.createElement('li');
+    Post.className = 'task';
+    Post.innerHTML = titlePost + "<button>Usuń</button>";
+    ul.appendChild(Post);
+    input.value = "";
+
+    //const liItems = document.querySelectorAll('li.task').length;
+    PostNumber.textContent = listItems.length;
+
+    Post.querySelector('button').addEventListener('click', removePost);
+}
+
+form.addEventListener('submit', addPost)
